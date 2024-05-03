191378a782a93b5f5dd0b2c3607c4ee15ffce2ad
Madelyn Bailey <83782902+madbailey@users.noreply.github.com>
2024-05-03 16:44:26 -0400

    more chat stuff

--- main.go
+++ main.go
@@ -38,10 +38,21 @@ func (s *Server) readLoop(ws *websocket.Conn) {
 			continue
 		}
 		msg := buf[:n]
-		fmt.Println(string(msg))
-		ws.Write([]byte("thank you for the message!"))
+
+		s.broadcast(msg)
 	}
 }
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
+
 func main() {
 	server := NewServer()
 	http.Handle("/ws", websocket.Handler(server.handleWS))
