62478357d51ef3d752141fa42e0f9c21a6282b58
Madelyn Bailey <83782902+madbailey@users.noreply.github.com>
2024-05-03 15:57:54 -0400

    simplifying

--- go.mod
+++ go.mod
@@ -2,6 +2,3 @@ module go_chat_server
 
 go 1.18
 
-require (
-	golang.org/x/net v0.17.0 // direct
-)
--- main.go
+++ main.go
@@ -1,61 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"net/http"
-	"os"
-
-	"golang.org/x/net/websocket"
 )
 
-type Server struct {
-	conns map[*websocket.Conn]bool
-}
-
-func NewServer() *Server {
-	return &Server{
-		conns: make(map[*websocket.Conn]bool),
-	}
-}
-
-func (s *Server) handleWS(ws *websocket.Conn) {
-	fmt.Println("New connection:", ws.RemoteAddr())
-	s.conns[ws] = true
-	s.readLoop(ws)
-}
-
-func (s *Server) readLoop(ws *websocket.Conn) {
-	buf := make([]byte, 1024)
-	for {
-		n, err := ws.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Connection closed:", ws.RemoteAddr())
-				break
-			}
-			fmt.Println("Read error:", err)
-			continue
-		}
-
-		msg := buf[:n]
-		fmt.Printf("Received message from %v: %s\n", ws.RemoteAddr(), msg)
-		responseMsg := "Thank you for your message!"
-		ws.Write([]byte(responseMsg))
-		fmt.Printf("Sent response to %v: %s\n", ws.RemoteAddr(), responseMsg)
-	}
-
-	// Remove the connection from the map
-	delete(s.conns, ws)
-	fmt.Println("Removed connection:", ws.RemoteAddr())
-}
-
 func main() {
-	server := NewServer()
-	http.Handle("/ws", websocket.Handler(server.handleWS))
-	fmt.Println("Server starting on http://localhost:8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Server failed: %v\n", err)
-	}
+	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+	http.ListenAndServe(":8080", nil)
 }
